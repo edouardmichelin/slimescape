78c21ea051fe9c2f9ee3b3b76b00d279a617bf60
NervzZ <julien.jordan@epfl.ch>
2022-03-11 00:14:24 +0100

    script - CollisionsExample - prints the given messages on the different collision states of the object

---
+++ Assets/Scripts/CollisionsExample.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionsExample : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+        print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+        print("Their relative velocity is " + collisionInfo.relativeVelocity);
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        print(gameObject.name + " and " + collisionInfo.collider.name + " are still colliding");
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        print(gameObject.name + " and " + collisionInfo.collider.name + " are no longer colliding");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
