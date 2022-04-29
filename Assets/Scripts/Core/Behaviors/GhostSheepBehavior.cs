26b6bc9fd07f88bc30c1728a41c590af2d17d584
NervzZ <nervzz@github.com>
2022-04-29 14:13:49 +0200

    Script - GhostSheepBehavior - on role switch, call a change of movement type for the players

--- Assets/Scripts/Core/Behaviors/GhostSheepBehavior.cs
+++ Assets/Scripts/Core/Behaviors/GhostSheepBehavior.cs
@@ -70,12 +70,14 @@ public class GhostSheepBehavior : AgentBehaviour
             gameObject.tag = ATTACKING_TAG;
             agent.SetVisualEffect(VisualEffect.VisualEffectConstAll, Color.red, 0);
             AudioManager.Instance.PlaySoundEffect(soundOnSwitchingToWolf);
+            GameManager.Instance.AllMoveOnStone();
         }
         else
         {
             gameObject.tag = FLEEING_TAG;
             agent.SetVisualEffect(VisualEffect.VisualEffectConstAll, Color.green, 0);
             AudioManager.Instance.PlaySoundEffect(soundOnSwitchingToSheep);
+            GameManager.Instance.AllMoveNormally();
         }
 
         float time = UnityEngine.Random.Range(Config.MIN_ROLE_TIME, Config.MAX_ROLE_TIME);
