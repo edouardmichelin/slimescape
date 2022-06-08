ffbffe7f85464faf9427ecf6c57ed75f458592d2
edouardmichelin <42861545+edouardmichelin@users.noreply.github.com>
2022-06-08 20:33:43 +0200

    Feature/card 82974942 (#17)
    * Script - Spillage

---
+++ Assets/Scripts/Game/Spillage.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Spillage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject obj = other.transform.parent.gameObject;
+        
+        if (obj.CompareTag(Config.TAG_DOG))
+        {
+            MoveWithKeyboardBehavior b = obj.GetComponent<MoveWithKeyboardBehavior>();
+            if (b == null)
+                return;
+
+            b.MoveOnStone();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        GameObject obj = other.transform.parent.gameObject;
+        
+        if (obj.CompareTag(Config.TAG_DOG))
+        {
+            MoveWithKeyboardBehavior b = obj.GetComponent<MoveWithKeyboardBehavior>();
+            if (b == null)
+                return;
+
+            b.MoveOnStone();
+        }
+    }
+}
